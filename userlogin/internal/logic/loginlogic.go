ac4b32408d96b888e97d671f003cc2f8327f4f92
shr26207 <sunhaoran@offcn.com>
2022-07-19 12:00:27 +0800

    logic login

---
+++ userlogin/internal/logic/loginlogic.go
@@ -0,0 +1,55 @@
+package logic
+
+import (
+	"context"
+	"time"
+
+	"google.golang.org/grpc/status"
+	"user-login/userlogin/common/cryptx"
+	"user-login/userlogin/common/jwtx"
+	"user-login/userlogin/internal/svc"
+	"user-login/userlogin/internal/types"
+	"user-login/userlogin/model"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type LoginLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic {
+	return &LoginLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *LoginLogic) Login(req *types.LoginRequest) (resp *types.LoginResponse, err error) {
+	res, err := l.svcCtx.UserModel.FindOneByEmail(l.ctx, req.Email)
+	if err != nil {
+		if err == model.ErrNotFound {
+			return nil, status.Error(100, "用户不存在")
+		}
+		return nil, status.Error(500, err.Error())
+	}
+	// 判断密码是否正确
+	password := cryptx.PasswordEncrypt(l.svcCtx.Config.Salt, req.Password)
+	if password != res.Password {
+		return nil, status.Error(100, "密码错误")
+	}
+	now := time.Now().Unix()
+	accessExpire := l.svcCtx.Config.Auth.AccessExpire
+
+	accessToken, err := jwtx.GetToken(l.svcCtx.Config.Auth.AccessSecret, now, accessExpire, res.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &types.LoginResponse{
+		Token:  accessToken,
+		Expire: now + accessExpire,
+	}, nil
+}
