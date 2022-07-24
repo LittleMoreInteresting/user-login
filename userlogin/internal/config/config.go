8704f3aeeae6d3e8099a73e00317adef7bab2527
sunhaoran <1208818417@qq.com>
2022-07-25 06:58:07 +0800

    中间件

--- userlogin/internal/config/config.go
+++ userlogin/internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/core/trace"
 	"github.com/zeromicro/go-zero/rest"
 )
 
@@ -19,4 +20,6 @@ type Config struct {
 		AccessExpire int64
 	}
 	LogConf logx.LogConf
+
+	Telemetry trace.Config
 }
